7f682c431bf02c41a305e6aa61606c93ab49aeaf
Jacek Achtelik <jacekachtelik@gmail.com>
2018-01-06 20:17:56 +0100

    m10t01 - Generatory

---
+++ client/components/Hellom.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+const Hellom = (props, context) => {
+  return (
+  );
+}
+
+Hellom.propTypes = {
+};
+
+export default Hellom;
---
+++ client/components/Hellos.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+const Hellos = (props, context) => {
+  return (
+  );
+}
+
+Hellos.propTypes = {
+};
+
+export default Hellos;
